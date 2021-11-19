8a07d1d24feaaeaeee47858d1129794e152dc8ad
Ying Huang <huangyingw@gmail.com>
2021-11-18 22:32:20 -0800

    n

--- mysql-init.sql
+++ mysql-init.sql
@@ -1,6 +1,16 @@
 CREATE DATABASE IF NOT EXISTS quotes;
 USE quotes;
-CREATE TABLE IF NOT EXISTS quotes (id int AUTO_INCREMENT PRIMARY KEY, quote varchar(255), author varchar(255));
+CREATE TABLE IF NOT EXISTS quotes (
+    id int AUTO_INCREMENT PRIMARY KEY, 
+    title      varchar(255),
+    movie_name varchar(255),
+    actress    varchar(255),
+    mosaic     varchar(255),
+    size       varchar(255),
+    torrents   text,
+    magnets    text,
+    link       varchar(255)
+);
 CREATE USER 'scraper'@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON *.* TO 'scraper'@'%';
 FLUSH PRIVILEGES;

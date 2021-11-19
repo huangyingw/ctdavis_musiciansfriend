d0c0b128da24ea7d9e8e5d1fe8c3ed68c05a1d2c
Ying Huang <huangyingw@gmail.com>
2021-11-18 22:32:57 -0800

    n

--- reset.sh
+++ reset.sh
@@ -4,4 +4,6 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 ~/loadrc/dockerrc/killDockers.sh
-rm -fr ./db_dir/
+rm -fr ./db_dir
+docker-compose up --build -d
+watch ~/loadrc/sqlrc/xsql.sh select.sql

c0f54d3a4cc416a677793b7a07d69127f815d4e3
Ying Huang <huangyingw@gmail.com>
2021-11-18 22:32:49 -0800

    n

--- create.sql
+++ create.sql
@@ -1,5 +1,11 @@
 CREATE TABLE IF NOT EXISTS quotes (
     id int AUTO_INCREMENT PRIMARY KEY, 
-    quote varchar(255), 
-    author varchar(255)
+    title      varchar(255),
+    movie_name varchar(255),
+    actress    varchar(255),
+    mosaic     varchar(255),
+    size       varchar(255),
+    torrents   text,
+    magnets    text,
+    link       varchar(255)
 );

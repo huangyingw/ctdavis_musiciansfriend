569213827a6ce54b16c16b46bdff2c2e5891ee22
Ying Huang <huangyingw@gmail.com>
2021-11-19 17:02:37 -0800

    n

--- quotes_spider/items.py
+++ quotes_spider/items.py
@@ -1,6 +1,13 @@
 import scrapy
-from scrapy.loader.processors import Join, MapCompose, TakeFirst
+from scrapy.loader.processors import TakeFirst
+
 
 class QuotesSpiderItem(scrapy.Item):
-    quote = scrapy.Field(output_processor=TakeFirst())
-    author = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(output_processor=TakeFirst())
+    movie_name = scrapy.Field(output_processor=TakeFirst())
+    actress = scrapy.Field(output_processor=TakeFirst())
+    mosaic = scrapy.Field(output_processor=TakeFirst())
+    size = scrapy.Field(output_processor=TakeFirst())
+    torrents = scrapy.Field(output_processor=TakeFirst())
+    magnets = scrapy.Field(output_processor=TakeFirst())
+    link = scrapy.Field(output_processor=TakeFirst())

e6725b7aa45640fe6e16915a3a792172fd49fbdb
Ying Huang <huangyingw@gmail.com>
2021-11-23 22:04:57 -0800

    n

---
+++ quotes_spider/spiders/quotes_spider.py.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+~/loadrc/dockerrc/killDockers.sh
+rm -fr ./db_dir
+docker-compose up --build -d
+watch ~/loadrc/sqlrc/xsql.sh select.sql
